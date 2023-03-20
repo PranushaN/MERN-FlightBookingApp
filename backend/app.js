418083c90dded01deb4bfd42c34139ce5fc50572
PranushaN <npranusha@gmail.com>
2023-03-20 20:37:45 +0530

    flights search api added

--- backend/app.js
+++ backend/app.js
@@ -5,12 +5,14 @@ const cors = require("cors");
 
 const HttpError = require('./models/http-error');
 const placesRouter = require('./routes/places-routes');
+const flightsRouter = require('./routes/flights-route');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
 app.use("/api/places", placesRouter);
+app.use("/api/flights", flightsRouter);
 
 app.use((req, res, next) => {
     const err = new HttpError("Couldn't find this route", 404);
@@ -25,7 +27,8 @@ app.use((error, req, res, next) => {
     res.json({message: error.message || "Unknown error occured!"});
 });
 
-mongoose.connect('mongodb+srv://admin:sherlock221b@makemytripcluster.htvcixo.mongodb.net/places?retryWrites=true&w=majority')
+mongoose.set('strictQuery', false);
+mongoose.connect('mongodb+srv://admin:sherlock221b@makemytripcluster.htvcixo.mongodb.net/MMT?retryWrites=true&w=majority')
     .then(() => {
         app.listen(5001);
     })
